854ff9475e5aa4c49b9cb02473e87a8152139492
Erik Shrestha <Eshrestha@student.neumont.edu>
2021-02-26 15:45:37 -0700

    Added player class

---
+++ Card Game Gallery/Models/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Card_Game_Gallery.Models
+{
+    [Serializable]
+    public class Player
+    {
+        public string name;
+        public List<Card> cards;
+        public int points;
+
+        public Player(string name, List<Card> cards, int points = 0)
+        {
+            this.name = name;
+            this.cards = cards;
+            this.points = points;
+        }
+    }
+}
