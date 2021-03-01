1211832023387913c9be91622a132a13c7875b5f
rshaw02 <rshaw@student.neumont.edu>
2021-03-01 16:15:13 -0700

    Added a method to Card for comparing face values.

--- Card Game Gallery/Models/Card.cs
+++ Card Game Gallery/Models/Card.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Text;
 
 namespace Card_Game_Gallery.Models
 {
     [Serializable]
-    public class Card
+    public class Card : IComparable, IComparable<Card>
     {
         public enum CardSuite
         {
@@ -17,7 +18,7 @@ namespace Card_Game_Gallery.Models
         /// </summary>
         public enum CardFace
         {
-            ACE = 1, TWO = 2, THREE = 3, FOUR = 4, FIVE = 5, SIX = 6, SEVEN = 7, EIGHT = 8, NINE = 9, TEN = 10, JACK = 11, QUEEN = 12, KING = 13 
+            ACE = 1, TWO = 2, THREE = 3, FOUR = 4, FIVE = 5, SIX = 6, SEVEN = 7, EIGHT = 8, NINE = 9, TEN = 10, JACK = 11, QUEEN = 12, KING = 13
         }
 
         public CardSuite Suite { get; set; }
@@ -38,6 +39,52 @@ namespace Card_Game_Gallery.Models
         public void Flip()
         {
             Revealed = !Revealed;
+        }
+
+        /// <summary>
+        /// Returns a numeric value representing which of the two cards are of a greater value based on their <c>Face</c>.
+        /// Returns 1 if this card is of a greater value than the other card, returns -1 if this card is of a lesser value, and returns 0 if both cards are of equal value.
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public int CompareCardFaceValues(Card other)
+        {
+            if (Face == CardFace.ACE || other.Face == CardFace.ACE)
+            {
+                return Face == other.Face ? 0 : (Face > other.Face ? -1 : 1);
+            }
+            else
+            {
+                return Face == other.Face ? 0 : (Face > other.Face ? 1 : -1);
+            }
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj is null)
+            {
+                return 1;
+            }
+            if (obj is Card)
+            {
+                return CompareTo(obj as Card);
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+        public int CompareTo([AllowNull] Card other)
+        {
+            if (other is null)
+            {
+                return 1;
+            }
+            else
+            {
+                return Face == other.Face ? 0 : (Face > other.Face ? 1 : -1);
+            }
         }
 
         private void HandleBlackjackValue()
