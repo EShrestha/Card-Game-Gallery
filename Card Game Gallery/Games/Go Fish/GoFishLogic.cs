7557983231759f00adf29fee2caafbcf276769a0
Erik Shrestha <Eshrestha@student.neumont.edu>
2021-03-01 16:47:20 -0700

    Some methods for Go Fish logic

--- Card Game Gallery/Games/Go Fish/GoFishLogic.cs
+++ Card Game Gallery/Games/Go Fish/GoFishLogic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Card_Game_Gallery.Models;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Runtime.Serialization;
@@ -9,12 +10,12 @@ namespace Card_Game_Gallery.Games.Go_Fish
 {
 
     // Holds all of the logic for Go Fish, such as winCheck...
-    public static class GoFishLogic
+    public class GoFishLogic
     {
         private static object saveGamePath;
 
 
-        // Saves teh current state of the game
+        // Saves the current state of the game
         public static bool SaveGame(GoFishSaveGame save, string saveGamePath)
         {
             try
@@ -37,6 +38,55 @@ namespace Card_Game_Gallery.Games.Go_Fish
             {
                 return false;
             }
+        }
+
+        // Checks if a player has a certain card (only face value)
+        public bool playerHasCard(GoFishPlayer player, Card card)
+        {
+            foreach(Card c in player.cards)
+            {
+                if (c.Face == card.Face)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+        //
+        public void getCardFromPlayer(GoFishPlayer askingPlayer, GoFishPlayer askedPlayer, Card card)
+        {
+            foreach(Card c in askedPlayer.cards) // Going through each card of the asked player's hand
+            {
+                if(c.Face == card.Face) // If the asked player's card matches the card the asking player wants...
+                {
+                    askingPlayer.matchedCards.Add(c); // Give card to asking player
+                    askedPlayer.cards.Remove(c); // Remove card from asked palyer
+                    seperateMatchingCards(askingPlayer);
+                }
+            }
+        }
+
+        // Seperates matching pairs into a different list
+        public void seperateMatchingCards(GoFishPlayer player)
+        {
+            for(int i = 0; i<player.cards.Count; i++)
+            {
+                for(int j = i+1; j<player.cards.Count; j++)
+                {
+                    if(player.cards[i] == player.cards[j])
+                    {
+                        player.matchedCards.Add(player.cards[i]); // Moving card 1 to matchedCards list
+                        player.matchedCards.Add(player.cards[j]); // Moving card 2 to matchedCards list
+                        // Removing form players hand after moving card to seperate list
+                        player.cards.RemoveAt(i);
+                        player.cards.RemoveAt(j);
+                        continue;
+                    }
+                }
+            }
+   
         }
 
     }
---
+++ Card Game Gallery/Models/GoFishPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Card_Game_Gallery.Models
+{
+    public class GoFishPlayer : Player
+    {
+        public List<Card> matchedCards;
+
+        public GoFishPlayer(string name, bool isAi = false, List<Card> cards = null, int score = 0) : base(name,isAi,cards,score)
+        {
+        }
+    }
+}
